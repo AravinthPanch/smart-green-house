aa89151288f38a15bfe6c6c55f9560f6f6e5a957
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2014-07-02 17:45:55 +0200

    Range sliders are added.

--- Source/UI/app/main.js
+++ Source/UI/app/main.js
@@ -1,6 +1,34 @@
 define(function (require) {	
 	var io = require('socketio');
 	
+    $( "#soilrange" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+    });
+	
+    $( "#temperaturerange" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+    });
+	
+    $( "#humidityrange" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+    });
+	
+    $( "#luminosityrange" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+    });
+	
 	io = io.connect()
 	io.emit('serial') 
 	io.on('logs', function(data) {
